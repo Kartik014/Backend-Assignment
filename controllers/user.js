2a50b577706f7a127291f3021a58fef93955015c
Kartik014 <99882037+Kartik014@users.noreply.github.com>
2024-01-26 17:29:19 +0530

    ADDING VOICE CALL

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,95 @@
 import { UserModel } from "../models/userModel.js";
+import { TaskModel } from "../models/taskModel.js";
 import jwt from "jsonwebtoken";
 import schedule from "node-schedule";
 import bcrypt from "bcrypt";
 import dotenv from "dotenv";
+import twilio from "twilio";
 dotenv.config({ path: "./Database/config.env" });
+
+const calculatePriority = (due_date) => {
+    const today = new Date()
+    const dueDate = new Date(due_date)
+
+    const day_difference = Math.ceil((dueDate - today) / (1000 * 60 * 60 * 24))
+    if (day_difference === 0) {
+        return 0
+    }
+    if (day_difference <= 2) {
+        return 1
+    }
+    if (day_difference <= 4) {
+        return 2
+    }
+    if (day_difference >= 5) {
+        return 3
+    }
+}
+
+const changeTaskPriority = async () => {
+    try {
+        const tasks = await TaskModel.find()
+
+        tasks.forEach(async (task) => {
+            task.priority = calculatePriority(task.due_date)
+            await task.save()
+        })
+        console.log('Task priorities updated successfully.')
+    } catch (err) {
+        console.error('Error updating task priorities:', err)
+    }
+}
+schedule.scheduleJob('0 0 * * *', changeTaskPriority)
+changeTaskPriority()
+
+const makeVoiceCall = async () => {
+    try {
+        const users = await UserModel.find().sort({ priority: 1 })
+        const currentDate = new Date()
+
+        for (const user of users) {
+            const tasks = await TaskModel.find({
+                user_id: user._id,
+                due_date: { $lt: currentDate },
+            }).sort({ priority: -1 })
+
+            for (const task of tasks) {
+                await makeCall(user.phone_number, task.title)
+                break;
+            }
+        }
+
+        console.log('Voice calls completed successfully.')
+    } catch (error) {
+        console.error('Error making voice calls:', error)
+    }
+};
+
+const makeCall = async (phoneNumber, taskTitle) => {
+    try {
+        const client = new twilio(process.env.TIWILIO_SID, process.env.TIWILIO_AUTH_TOKEN)
+        const isValidPhoneNumber = await client.lookups.phoneNumbers(phoneNumber)
+            .fetch()
+            .then(phoneNumber => true)
+            .catch(error => false)
+
+        if (!isValidPhoneNumber) {
+            console.error(`Invalid phone number: ${phoneNumber}`);
+            return;
+        }
+        await client.calls.create({
+            to: phoneNumber,
+            from: process.env.TIWILIO_PHONE_NUMBER,
+            url: process.env.TIWILIO_URL,
+        });
+
+        console.log(`Voice call made to ${phoneNumber} for task: ${taskTitle}`)
+    } catch (error) {
+        console.error('Error making voice call:', error)
+    }
+}
+schedule.scheduleJob('0 * * * *', makeVoiceCall)
+makeVoiceCall()
 
 const generateToken = (user_id) => {
     const secretKey = process.env.SECRET_KEY
@@ -83,19 +169,3 @@ export const logIn = async (req, res) => {
         })
     }
 }
-
-const changeTaskPriority = async () => {
-    try {
-        const tasks = await TaskModel.find();
-
-        tasks.forEach(async (task) => {
-            task.priority = calculatePriority(task.due_date);
-            await task.save();
-        })
-        console.log('Task priorities updated successfully.')
-    } catch (err) {
-        console.error('Error updating task priorities:', err)
-    }
-}
-schedule.scheduleJob('0 0 * * *', changeTaskPriority)
-changeTaskPriority()
